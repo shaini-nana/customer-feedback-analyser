b552c0d84537c65b76503e9745b0a61b36f7c0aa
shaini-nana <shaini.nanayakkara@gmail.com>
2021-04-04 07:32:15 +0530

    extracting out AWS code to a new file

--- SentimentAnalyzer/sentimentAnalyzer.js
+++ SentimentAnalyzer/sentimentAnalyzer.js
@@ -1,24 +1,8 @@
-const AWS = require("aws-sdk");
-
-AWS.config.loadFromPath('./config.json');
-
-const comprehend = new AWS.Comprehend({
-  apiVersion: '2017-11-27'
-});
+const aws = require('./aws.js');
 
 const params = {
   LanguageCode: 'en',
   Text: 'Food is really good and tasty'
 };
 
-
-const analyzeSentiment = sentiment => {
-
-  comprehend.detectSentiment(sentiment).promise().then(async data => {
-    console.log(`Success Result.... : ${JSON.stringify(data)}`);
-  }).catch(error => {
-    console.log(`Error occurred... ${error.stack}`)
-  })
-};
-
-analyzeSentiment(params);
+aws.analyzeSentiment(params);
