b47332cb3ab2c6362f833801781fb925d82a5abf
shaini-nana <shaini.nanayakkara@gmail.com>
2021-04-04 07:40:16 +0530

    file reader added

--- SentimentAnalyzer/sentimentAnalyzer.js
+++ SentimentAnalyzer/sentimentAnalyzer.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+var parse = require('csv-parse');
+
 const aws = require('./aws.js');
 
 const params = {
@@ -5,4 +8,18 @@ const params = {
   Text: 'Food is really good and tasty'
 };
 
+const reviewsToBeAnalyzed = [];
+
+const readCSV = pathToFile => {
+  fs.createReadStream(pathToFile)
+    .pipe(parse({delimiter: ':'}))
+    .on('data', (row) => {
+      reviewsToBeAnalyzed.push(row[0])
+    })
+    .on('end', async () => {
+      console.log(`CSV file successfully processed. Number of reviews to be processed: ${reviewsToBeAnalyzed.length}`);
+    });
+};
+
+readCSV("../DataScraper/reviews_01.csv");
 aws.analyzeSentiment(params);
