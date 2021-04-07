7e2d0292c0bd8ce4dfc049c28646d2df4d8d83fa
shaini-nana <shaini.nanayakkara@gmail.com>
2021-04-08 01:51:32 +0530

    frontend minor code cleanups

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -1,5 +1,4 @@
 import React  from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Alert, Form, Input, Button } from 'antd';
 import { MailOutlined, LockOutlined } from '@ant-design/icons';
--- frontend/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import dashboardReducer from './dashboard.store';
-
-export default configureStore({
-  reducer: {
-    dashboard: dashboardReducer,
-  },
-});
