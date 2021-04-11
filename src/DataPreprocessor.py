c58f6adab070e97eb3c32d510675f895358cd81f
shaini-nana <shaini.nanayakkara@gmail.com>
2021-04-11 10:40:56 +0530

    commited changes on the preprocessing setps to maintain food wise advance and non advance steps

--- src/DataPreprocessor.py
+++ src/DataPreprocessor.py
@@ -46,13 +46,14 @@ for row in reviewReader:
     for reviewSentence in reviewSentences:
         processedSentence = reviewSentence.lower()
         # lemmatize each review sentence
-        processedReview = lemmatize_sentence(processedSentence)
+        processedSentenceWithAdvancePreprocessing = lemmatize_sentence(processedSentence)
 
         # looping each review sentence through each food item
         # to check if the review sentence is about any of the identified food items
         for inx, foodItem in enumerate(foodItems):
             if foodItem in  processedSentence:
                 foodReviews[inx].append(processedSentence)
+                foodReviewsAdvance[inx].append(processedSentenceWithAdvancePreprocessing)
 
 for reviewCategories in foodReviews:
     if len(reviewCategories) != 0:
@@ -97,5 +98,17 @@ if isCategorizedReviews:
             # write each food item specific reviews to the respective food item csv file
             for foodReview in foodReviews[inx]:
                 writer.writerow([foodReview])
+
+    Path("../reviews/processedReviews/" + selectedReviewsFile + "/advance").mkdir(parents=True, exist_ok=True)
+
+    # loop through each food item to create specific directories to get the reviews
+    for inx, foodItem in enumerate(foodItems):
+        # create/open csv files specific to each food item
+        with open("../reviews/processedReviews/" + selectedReviewsFile + "/advance" + "/" + foodItem + ".csv", 'w', newline='') as file:
+            writer = csv.writer(file)
+            # write each food item specific reviews to the respective food item csv file
+            for foodReview in foodReviewsAdvance[inx]:
+                writer.writerow([foodReview])
+
 else:
     print("Filtered food reviews NOT found !!!")
