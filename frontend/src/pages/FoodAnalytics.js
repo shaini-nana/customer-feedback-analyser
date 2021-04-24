cba7d7a0db6159d1047f09bdee37444977f9b098
shaini-nana <shaini.nanayakkara@gmail.com>
2021-04-24 10:38:38 +0530

    enhancing item analytics ui

--- frontend/src/pages/FoodAnalytics.js
+++ frontend/src/pages/FoodAnalytics.js
@@ -97,31 +97,39 @@ class FoodAnalytics extends Component {
 
     return (
       <Container maxWidth={false}>
-        <Grid
-          container
-          spacing={3}
+        <Box
+          sx={{
+            backgroundColor: 'background.default',
+            minHeight: '100%',
+            py: 3
+          }}
         >
           <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
+            container
+            spacing={3}
           >
-            <Budget
-              score={
-                this.state.apiResponse ? this.state.apiResponse.mixedReviewCount : 0
-              }
-              totalScore={
-                this.state.apiResponse ? this.state.apiResponse.totalNumberOfReviews : 0
-              }
-              cardTitle="Worst Performing Item:"
-              colour={red[600]}
-              isTrend={true}
-              trend={startCase(toLower(worstItem.foodItem))}
-            />
+            <Grid
+              item
+              lg={3}
+              sm={6}
+              xl={3}
+              xs={12}
+            >
+              <Budget
+                score={
+                  this.state.apiResponse ? this.state.apiResponse.mixedReviewCount : 0
+                }
+                totalScore={
+                  this.state.apiResponse ? this.state.apiResponse.totalNumberOfReviews : 0
+                }
+                cardTitle="Worst Performing Item:"
+                colour={red[600]}
+                isTrend={true}
+                trend={startCase(toLower(worstItem.foodItem))}
+              />
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
       </Container>
     );
   }
